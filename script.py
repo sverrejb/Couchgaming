828de8566d60b5241664e4fc4db0b2ea0de8cc8a
Sverre Johann Bjørke <sverrejb@gmail.com>
2025-01-03 13:19:30 +0100

    add rumble

--- script.py
+++ script.py
@@ -1,15 +1,20 @@
 #! /usr/bin/python3
 
 import evdev
-from evdev import InputDevice, ecodes, list_devices
+from evdev import InputDevice, ecodes, list_devices, ff
 import time
 import subprocess
 import random
 import socket
 
-
+MAC_ADDRESS_DESKTOP = 'd8:80:83:9c:5b:2f'
+MAC_ADDRESS_TV = '48:8d:36:bc:e8:ec'
 DEVICE_PATH = '/dev/input/event14'
 RETRY_INTERVAL = 3
+BTN_A = ecodes.BTN_SOUTH 
+BTN_B = ecodes.BTN_EAST
+COMBO_HOLD_TIME = 1.5 # Seconds
+HDMI_INPUT = 'HDMI_2'
 
 def find_controller_device():
     while True:
@@ -25,21 +30,18 @@ def find_controller_device():
             print(f"Error checking devices: {e}")
             time.sleep(RETRY_INTERVAL)
 
-device = find_controller_device()
-
-BTN_A = ecodes.BTN_SOUTH 
-BTN_B = ecodes.BTN_EAST
-
-pressed_buttons = set()
-combo_pressed_time = None
-COMBO_HOLD_TIME = 3  # Seconds
-
 def send_wol_packet(mac_address):
-       mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
-       magic_packet = b'\xff' * 6 + mac_bytes * 16
-       with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-           sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-           sock.sendto(magic_packet, ('<broadcast>', 9))
+    mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
+    magic_packet = b'\xff' * 6 + mac_bytes * 16
+    for _ in range(10):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.sendto(magic_packet, ('<broadcast>', 9))
+
+def wake_screen():
+    x, y = random.randint(0, 10), random.randint(0, 10)
+    subprocess.run(["ydotool", "mousemove", str(x), str(y)])
+
 
 def start_steamlink():
     try:
@@ -51,6 +53,8 @@ def start_steamlink():
 
         # If pgrep finds an existing process, it returns a non-empty result
         if result.stdout:
+            print("Bringing SteamLink to the foreground...")
+            subprocess.run(["swaymsg", "[app_id=\"com.valvesoftware.SteamLink\"] focus"])
             print("SteamLink is already running, skipping launch.")
         else:
             # If SteamLink is not running, start it
@@ -71,21 +75,57 @@ def start_steamlink():
     except Exception as e:
         print(f"Error checking for SteamLink process: {e}")
 
+def rumble_controller(device):
+    rumble = ff.Rumble(strong_magnitude=0xc000, weak_magnitude=0xc000)
+    duration_ms = 500
+
+    effect = ff.Effect(
+        ecodes.FF_RUMBLE, # type
+        -1, # id (set by ioctl)
+        0,
+        ff.Trigger(0, 0), # no triggers
+        ff.Replay(duration_ms, 0), # length and delay
+        ff.EffectType(ff_rumble_effect=rumble)
+    )
+    
+    effect_id = device.upload_effect(effect)
+    repeat_count = 1
+    device.write(ecodes.EV_FF, effect_id, repeat_count)
+    time.sleep(0.5)
+    device.erase_effect(effect_id) 
+
 def execute_command():
     print("Button combo detected! Running script...")
-    send_wol_packet("d8:80:83:9c:5b:2f")  # Replace with the actual MAC address of DESKTOP-L87EPJN
-
-    x, y = random.randint(0, 10), random.randint(0, 10)
-    subprocess.run(["ydotool", "mousemove", str(x), str(y)])
-
+    rumble_controller(device)
+    send_wol_packet(MAC_ADDRESS_TV)
+    send_wol_packet(MAC_ADDRESS_DESKTOP) 
+    wake_screen()
     start_steamlink()
-
-    for _ in range(3):
-        subprocess.run(["/home/sverrejb/.local/bin/alga", "power", "on"])
-        time.sleep(0.5)
     
-    time.sleep(5)
-    subprocess.run(["/home/sverrejb/.local/bin/alga", "input", "set", "HDMI_2"])
+    try:
+        subprocess.run(["/home/sverrejb/.local/bin/alga", "input", "set", HDMI_INPUT], check=True)
+    except:
+        print('Initial HDMI-switch failed. Trying harder.')
+        while True:
+            response = subprocess.run(["ping", "-c", "1", "-W", "1", "192.168.1.43"], stdout=subprocess.PIPE)
+            if response.returncode == 0:
+                print("TV is responding to ping.")
+                break
+            else:
+                print("Waiting for TV to respond to ping...")
+                time.sleep(1)
+        time.sleep(10)
+        
+        for i in range(5):
+            print(f'Setting HDMI-input {i+1}/5')
+            subprocess.run(["/home/sverrejb/.local/bin/alga", "input", "set", HDMI_INPUT])
+            time.sleep(0.2)
+        
+    print('Steps completed.')
+
+device = find_controller_device()
+pressed_buttons = set()
+combo_pressed_time = None
 
 try:
     while True:
@@ -107,6 +147,7 @@ try:
 
                 # Check if combo is held long enough
                 if combo_pressed_time and (time.time() - combo_pressed_time >= COMBO_HOLD_TIME):
+                    
                     execute_command()
                     combo_pressed_time = None  # Reset timer after execution
 
